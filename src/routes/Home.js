5f9357937de8a5c446b02fec6d02601b398a5e93
codingpalette <codingpalette@gmail.com>
2020-04-25 14:53:24 +0900

    HomePage.js 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,47 +1,12 @@
 // Home.js
 
 import React from 'react';
-import styled from 'styled-components';
-import ItemList from '../components/ItemList';
-
-const ListBox = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-`;
-
-const List = styled.li`
-  width: 100%;
-  padding: 10px;
-  box-sizing: border-box;
-  @media screen and (min-width: 768px) {
-    width: 33.33%;
-  }
-
-  @media screen and (min-width: 1200px) {
-    width: 25%;
-  }
-`;
+import HomePage from '../containers/HomePage';
 
 const Home = () => {
   return (
     <>
-      <ListBox>
-        <List>
-          <ItemList />
-        </List>
-        <List>
-          <ItemList />
-        </List>
-        <List>
-          <ItemList />
-        </List>
-        <List>
-          <ItemList />
-        </List>
-        <List>
-          <ItemList />
-        </List>
-      </ListBox>
+      <HomePage />
     </>
   );
 };
