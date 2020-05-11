e730c3a9e2c61240b3bc3427e5dffb7181b70d05
codingpalette <codingpalette@gmail.com>
2020-05-11 16:27:24 +0900

    디테일 페이지 수정

--- src/containers/DetailPage.js
+++ src/containers/DetailPage.js
@@ -22,12 +22,58 @@ const BackImage = styled.div`
   z-index: 0;
 `;
 
+const Content = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  z-index: 100;
+  position: relative;
+`;
+
+const ImageBox = styled.div`
+  width: 100%;
+  border-radius: 10px;
+  overflow: hidden;
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+  @media screen and (min-width: 768px) {
+    width: 40%;
+  }
+`;
+
+const TextBox = styled.div`
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    width: 55%;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 24px;
+  margin-bottom: 10px;
+  @media screen and (min-width: 768px) {
+    font-size: 32px;
+  }
+`;
+
+const Overview = styled.div`
+  font-size: 14px;
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
 function DetailPage(props) {
   const [data, setData] = useState('');
   const [loding, setLoding] = useState(false);
 
   const getDetail = async () => {
     const res = await detailGet(props.match.params.id);
+    console.log(res);
     setData(res.data);
     setLoding(true);
   };
@@ -41,6 +87,15 @@ function DetailPage(props) {
         {loding && (
           <>
             <BackImage bgImage={`https://image.tmdb.org/t/p/original${data.backdrop_path}`} />
+            <Content>
+              <ImageBox>
+                <img src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`} alt="커버 이미지" />
+              </ImageBox>
+              <TextBox>
+                <Title>{data.title}</Title>
+                <Overview>{data.overview}</Overview>
+              </TextBox>
+            </Content>
           </>
         )}
       </Container>
